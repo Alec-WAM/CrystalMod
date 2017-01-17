f4af7dc1cf364684bf260e8e4315ac2e66504987
Alec-WAM <tlmathisen2@gmail.com>
2017-01-17 10:50:30 -0600

    Added Metal Bars for each ingot

---
+++ src/main/java/alec_wam/CrystalMod/blocks/BlockMetalBars.java
@@ -0,0 +1,200 @@
+package alec_wam.CrystalMod.blocks;
+
+import java.util.List;
+import java.util.Locale;
+
+import alec_wam.CrystalMod.CrystalMod;
+import net.minecraft.block.BlockPane;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.block.statemap.StateMapperBase;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.IStringSerializable;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Rotation;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class BlockMetalBars extends BlockPane implements ICustomModel 
+{
+    public static final PropertyEnum<EnumMetalBarType> TYPE = PropertyEnum.<EnumMetalBarType>create("type", EnumMetalBarType.class);
+
+    public static enum EnumMetalBarType implements IStringSerializable, alec_wam.CrystalMod.blocks.EnumBlock.IEnumMeta{
+		DARK_IRON, BLUE, RED, GREEN, DARK, PURE;
+
+		final int meta;
+		
+		EnumMetalBarType(){
+			meta = ordinal();
+		}
+		
+		@Override
+		public int getMeta() {
+			return meta;
+		}
+
+		@Override
+		public String getName() {
+			return this.toString().toLowerCase(Locale.US);
+		}
+
+		public static EnumMetalBarType byMetadata(int meta) {
+			return values()[meta % values().length];
+		}
+		
+	}
+    
+    public BlockMetalBars()
+    {
+        super(Material.IRON, true);
+        this.setDefaultState(this.blockState.getBaseState().withProperty(NORTH, Boolean.valueOf(false)).withProperty(EAST, Boolean.valueOf(false)).withProperty(SOUTH, Boolean.valueOf(false)).withProperty(WEST, Boolean.valueOf(false)).withProperty(TYPE, EnumMetalBarType.DARK_IRON));
+        this.setCreativeTab(CrystalMod.tabBlocks);
+    }
+
+    @SideOnly(Side.CLIENT)
+	public void initModel() {
+		ModelLoader.setCustomStateMapper(this, new CustomBlockStateMapper());
+		for(EnumMetalBarType type : EnumMetalBarType.values()){
+			String nameOverride = getRegistryName().getResourcePath() + "_" + type.getName();
+			ResourceLocation baseLocation = nameOverride == null ? getRegistryName() : new ResourceLocation("crystalmod", nameOverride);
+			ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), type.getMeta(), new ModelResourceLocation(baseLocation, "inventory"));
+		}
+	}
+    
+    /**
+     * Gets the metadata of the item this Block can drop. This method is called when the block gets destroyed. It
+     * returns the metadata of the dropped item based on the old metadata of the block.
+     */
+    public int damageDropped(IBlockState state)
+    {
+        return ((EnumMetalBarType)state.getValue(TYPE)).getMeta();
+    }
+
+    /**
+     * returns a list of blocks with the same ID, but different meta (eg: wood returns 4 blocks)
+     */
+    @SideOnly(Side.CLIENT)
+    public void getSubBlocks(Item itemIn, CreativeTabs tab, List<ItemStack> list)
+    {
+        for (int i = 0; i < EnumMetalBarType.values().length; ++i)
+        {
+            list.add(new ItemStack(itemIn, 1, i));
+        }
+    }
+
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getBlockLayer()
+    {
+        return BlockRenderLayer.CUTOUT_MIPPED;
+    }
+
+    /**
+     * Convert the given metadata into a BlockState for this Block
+     */
+    public IBlockState getStateFromMeta(int meta)
+    {
+        return this.getDefaultState().withProperty(TYPE, EnumMetalBarType.byMetadata(meta));
+    }
+
+    /**
+     * Convert the BlockState into the correct metadata value
+     */
+    public int getMetaFromState(IBlockState state)
+    {
+        return ((EnumMetalBarType)state.getValue(TYPE)).getMeta();
+    }
+
+    /**
+     * Returns the blockstate with the given rotation from the passed blockstate. If inapplicable, returns the passed
+     * blockstate.
+     */
+    public IBlockState withRotation(IBlockState state, Rotation rot)
+    {
+        switch (rot)
+        {
+            case CLOCKWISE_180:
+                return state.withProperty(NORTH, state.getValue(SOUTH)).withProperty(EAST, state.getValue(WEST)).withProperty(SOUTH, state.getValue(NORTH)).withProperty(WEST, state.getValue(EAST));
+            case COUNTERCLOCKWISE_90:
+                return state.withProperty(NORTH, state.getValue(EAST)).withProperty(EAST, state.getValue(SOUTH)).withProperty(SOUTH, state.getValue(WEST)).withProperty(WEST, state.getValue(NORTH));
+            case CLOCKWISE_90:
+                return state.withProperty(NORTH, state.getValue(WEST)).withProperty(EAST, state.getValue(NORTH)).withProperty(SOUTH, state.getValue(EAST)).withProperty(WEST, state.getValue(SOUTH));
+            default:
+                return state;
+        }
+    }
+
+    /**
+     * Returns the blockstate with the given mirror of the passed blockstate. If inapplicable, returns the passed
+     * blockstate.
+     */
+    public IBlockState withMirror(IBlockState state, Mirror mirrorIn)
+    {
+        switch (mirrorIn)
+        {
+            case LEFT_RIGHT:
+                return state.withProperty(NORTH, state.getValue(SOUTH)).withProperty(SOUTH, state.getValue(NORTH));
+            case FRONT_BACK:
+                return state.withProperty(EAST, state.getValue(WEST)).withProperty(WEST, state.getValue(EAST));
+            default:
+                return super.withMirror(state, mirrorIn);
+        }
+    }
+
+    protected BlockStateContainer createBlockState()
+    {
+        return new BlockStateContainer(this, new IProperty[] {NORTH, EAST, WEST, SOUTH, TYPE});
+    }
+    
+    public static class CustomBlockStateMapper extends StateMapperBase
+	{
+		@Override
+		protected ModelResourceLocation getModelResourceLocation(IBlockState state)
+		{
+			EnumMetalBarType type = state.getValue(TYPE);
+			StringBuilder builder = new StringBuilder();
+			String nameOverride = null;
+			
+			builder.append(NORTH.getName());
+			builder.append("=");
+			builder.append(state.getValue(NORTH));
+			
+			builder.append(",");
+			
+			builder.append(EAST.getName());
+			builder.append("=");
+			builder.append(state.getValue(EAST));
+			
+			builder.append(",");
+			
+			builder.append(WEST.getName());
+			builder.append("=");
+			builder.append(state.getValue(WEST));
+			
+			builder.append(",");
+			
+			builder.append(SOUTH.getName());
+			builder.append("=");
+			builder.append(state.getValue(SOUTH));
+			
+			nameOverride = state.getBlock().getRegistryName().getResourcePath() + "_" + type.getName();
+
+			if(builder.length() == 0)
+			{
+				builder.append("normal");
+			}
+
+			ResourceLocation baseLocation = nameOverride == null ? state.getBlock().getRegistryName() : new ResourceLocation("crystalmod", nameOverride);
+			
+			return new ModelResourceLocation(baseLocation, builder.toString());
+		}
+	}
+}
