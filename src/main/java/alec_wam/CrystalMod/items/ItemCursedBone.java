c41b372753540737301b0bda330cbc0e443023bf
Alec-WAM <tlmathisen2@gmail.com>
2017-03-09 10:46:17 -0600

    Finally Synced

--- src/main/java/alec_wam/CrystalMod/items/ItemCursedBone.java
+++ src/main/java/alec_wam/CrystalMod/items/ItemCursedBone.java
@@ -1,61 +1,6 @@
 package alec_wam.CrystalMod.items;
-
 import alec_wam.CrystalMod.CrystalMod;
 import alec_wam.CrystalMod.blocks.ICustomModel;
-<<<<<<< HEAD
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.IStringSerializable;
-import net.minecraft.util.NonNullList;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-
-public class ItemCursedBone extends Item implements ICustomModel {
-
-	public ItemCursedBone(){
-		setHasSubtypes(true);
-		setMaxDamage(0);
-		setCreativeTab(CrystalMod.tabItems);
-		ModItems.registerItem(this, "cursedbone");
-	}
-	
-	@SideOnly(Side.CLIENT)
-    public void initModel() {
-        for(BoneType type : BoneType.values()){
-        	 ModelLoader.setCustomModelResourceLocation(this, type.getMetadata(), new ModelResourceLocation(getRegistryName(), type.getUnlocalizedName()));
-        }
-    }
-	
-	@Override
-	public String getUnlocalizedName(ItemStack stack)
-    {
-        int i = stack.getMetadata();
-        return super.getUnlocalizedName() + "." + BoneType.byMetadata(i).getUnlocalizedName();
-    }
-	
-	@SideOnly(Side.CLIENT)
-	@Override
-    public void getSubItems(Item itemIn, CreativeTabs tab, NonNullList<ItemStack> subItems)
-    {
-        for (int i = 0; i < BoneType.values().length; ++i)
-        {
-            subItems.add(new ItemStack(itemIn, 1, i));
-        }
-    }
-	
-	@Override
-	public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
-    {
-=======
 import alec_wam.CrystalMod.util.ItemStackTools;
 import alec_wam.CrystalMod.util.ItemUtil;
 import net.minecraft.block.BlockNetherWart;
@@ -132,7 +77,6 @@ public class ItemCursedBone extends Item implements ICustomModel {
 		        }
 			}
 		}
->>>>>>> refs/remotes/origin/master
 		return EnumActionResult.PASS;
     }
 	
