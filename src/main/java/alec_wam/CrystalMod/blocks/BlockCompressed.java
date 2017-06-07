d782d2d55bb45842a635f68a206eb1b83fc78a19
Alec-WAM <tlmathisen2@gmail.com>
2017-06-06 22:38:38 -0500

    Gunpowder Explosion
    When you use a flint and steel on a 2x2 cube of compressed gunpowder it
    will explode

--- src/main/java/alec_wam/CrystalMod/blocks/BlockCompressed.java
+++ src/main/java/alec_wam/CrystalMod/blocks/BlockCompressed.java
@@ -4,14 +4,20 @@ import javax.annotation.Nullable;
 
 import alec_wam.CrystalMod.CrystalMod;
 import alec_wam.CrystalMod.blocks.EnumBlock.IEnumMeta;
+import alec_wam.CrystalMod.util.ItemStackTools;
+import alec_wam.CrystalMod.util.ModLogger;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Explosion;
@@ -37,6 +43,246 @@ public class BlockCompressed extends EnumBlock<BlockCompressed.CompressedBlockTy
 		for(CompressedBlockType type : CompressedBlockType.values())
 	        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), type.getMeta(), new ModelResourceLocation(this.getRegistryName(), TYPE.getName()+"="+type.getName()));
 	}
+	
+	@Override
+ 	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+		ItemStack held = playerIn.getHeldItem(hand);
+		CompressedBlockType type = state.getValue(TYPE);
+		if(type == CompressedBlockType.GUNPOWDER){			
+			if(ItemStackTools.isValid(held) && held.getItem() == Items.FLINT_AND_STEEL){
+				for(int i = 0; i < 8; i++){
+					if(i == 0){
+						IBlockState left = worldIn.getBlockState(pos.west());
+						IBlockState leftSouth = worldIn.getBlockState(pos.west().south());
+						IBlockState south = worldIn.getBlockState(pos.south());
+						IBlockState down = worldIn.getBlockState(pos.down());
+						IBlockState leftD = worldIn.getBlockState(pos.down().west());
+						IBlockState leftDSouth = worldIn.getBlockState(pos.down().west().south());
+						IBlockState southD = worldIn.getBlockState(pos.down().south());
+						if(left.getBlock() == this && left.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+							if(leftSouth.getBlock() == this && leftSouth.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+								if(south.getBlock() == this && south.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+									if(down.getBlock() == this && down.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+										if(leftD.getBlock() == this && leftD.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+											if(leftDSouth.getBlock() == this && leftDSouth.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+												if(southD.getBlock() == this && southD.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+													worldIn.createExplosion(playerIn, pos.getX() - 1, pos.getY() - 1, pos.getZ() + 1, 5.0f, true);
+													if(!playerIn.capabilities.isCreativeMode){
+														held.damageItem(1, playerIn);
+													}
+													return true;
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+					if(i == 1){
+						IBlockState right = worldIn.getBlockState(pos.east());
+						IBlockState rightSouth = worldIn.getBlockState(pos.east().south());
+						IBlockState south = worldIn.getBlockState(pos.south());
+						IBlockState down = worldIn.getBlockState(pos.down());
+						IBlockState rightD = worldIn.getBlockState(pos.down().east());
+						IBlockState rightDSouth = worldIn.getBlockState(pos.down().east().south());
+						IBlockState southD = worldIn.getBlockState(pos.down().south());
+						if(right.getBlock() == this && right.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+							if(rightSouth.getBlock() == this && rightSouth.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+								if(south.getBlock() == this && south.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+									if(down.getBlock() == this && down.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+										if(rightD.getBlock() == this && rightD.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+											if(rightDSouth.getBlock() == this && rightDSouth.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+												if(southD.getBlock() == this && southD.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+													worldIn.createExplosion(playerIn, pos.getX() - 1, pos.getY() - 1, pos.getZ() + 1, 5.0f, true);
+													if(!playerIn.capabilities.isCreativeMode){
+														held.damageItem(1, playerIn);
+													}
+													return true;
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+					if(i == 2){
+						IBlockState left = worldIn.getBlockState(pos.west());
+						IBlockState leftNorth = worldIn.getBlockState(pos.west().north());
+						IBlockState north = worldIn.getBlockState(pos.north());
+						IBlockState down = worldIn.getBlockState(pos.down());
+						IBlockState leftD = worldIn.getBlockState(pos.down().west());
+						IBlockState leftDNorth = worldIn.getBlockState(pos.down().west().north());
+						IBlockState northD = worldIn.getBlockState(pos.down().north());
+						if(left.getBlock() == this && left.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+							if(leftNorth.getBlock() == this && leftNorth.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+								if(north.getBlock() == this && north.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+									if(down.getBlock() == this && down.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+										if(leftD.getBlock() == this && leftD.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+											if(leftDNorth.getBlock() == this && leftDNorth.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+												if(northD.getBlock() == this && northD.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+													worldIn.createExplosion(playerIn, pos.getX() + 1, pos.getY() - 1, pos.getZ() + 1, 5.0f, true);
+													if(!playerIn.capabilities.isCreativeMode){
+														held.damageItem(1, playerIn);
+													}
+													return true;
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+					if(i == 3){
+						IBlockState right = worldIn.getBlockState(pos.east());
+						IBlockState rightNorth = worldIn.getBlockState(pos.east().north());
+						IBlockState north = worldIn.getBlockState(pos.north());
+						IBlockState down = worldIn.getBlockState(pos.down());
+						IBlockState rightD = worldIn.getBlockState(pos.down().east());
+						IBlockState rightDNorth = worldIn.getBlockState(pos.down().east().north());
+						IBlockState northD = worldIn.getBlockState(pos.down().north());
+						if(right.getBlock() == this && right.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+							if(rightNorth.getBlock() == this && rightNorth.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+								if(north.getBlock() == this && north.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+									if(down.getBlock() == this && down.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+										if(rightD.getBlock() == this && rightD.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+											if(rightDNorth.getBlock() == this && rightDNorth.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+												if(northD.getBlock() == this && northD.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+													worldIn.createExplosion(playerIn, pos.getX() + 1, pos.getY() - 1, pos.getZ() - 1, 5.0f, true);
+													if(!playerIn.capabilities.isCreativeMode){
+														held.damageItem(1, playerIn);
+													}
+													return true;
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+
+					//UP
+					if(i == 4){
+						IBlockState left = worldIn.getBlockState(pos.west());
+						IBlockState leftSouth = worldIn.getBlockState(pos.west().south());
+						IBlockState south = worldIn.getBlockState(pos.south());
+						IBlockState up = worldIn.getBlockState(pos.up());
+						IBlockState leftU = worldIn.getBlockState(pos.up().west());
+						IBlockState leftUSouth = worldIn.getBlockState(pos.up().west().south());
+						IBlockState southU = worldIn.getBlockState(pos.up().south());
+						if(left.getBlock() == this && left.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+							if(leftSouth.getBlock() == this && leftSouth.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+								if(south.getBlock() == this && south.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+									if(up.getBlock() == this && up.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+										if(leftU.getBlock() == this && leftU.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+											if(leftUSouth.getBlock() == this && leftUSouth.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+												if(southU.getBlock() == this && southU.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+													worldIn.createExplosion(playerIn, pos.getX() - 1, pos.getY() + 1, pos.getZ() + 1, 5.0f, true);
+													if(!playerIn.capabilities.isCreativeMode){
+														held.damageItem(1, playerIn);
+													}
+													return true;
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+					if(i == 5){
+						IBlockState right = worldIn.getBlockState(pos.east());
+						IBlockState rightSouth = worldIn.getBlockState(pos.east().south());
+						IBlockState south = worldIn.getBlockState(pos.south());
+						IBlockState up = worldIn.getBlockState(pos.up());
+						IBlockState rightU = worldIn.getBlockState(pos.up().east());
+						IBlockState rightUSouth = worldIn.getBlockState(pos.up().east().south());
+						IBlockState southU = worldIn.getBlockState(pos.up().south());
+						if(right.getBlock() == this && right.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+							if(rightSouth.getBlock() == this && rightSouth.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+								if(south.getBlock() == this && south.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+									if(up.getBlock() == this && up.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+										if(rightU.getBlock() == this && rightU.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+											if(rightUSouth.getBlock() == this && rightUSouth.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+												if(southU.getBlock() == this && southU.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+													worldIn.createExplosion(playerIn, pos.getX() - 1, pos.getY() + 1, pos.getZ() + 1, 5.0f, true);
+													if(!playerIn.capabilities.isCreativeMode){
+														held.damageItem(1, playerIn);
+													}
+													return true;
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+					if(i == 6){
+						IBlockState left = worldIn.getBlockState(pos.west());
+						IBlockState leftNorth = worldIn.getBlockState(pos.west().north());
+						IBlockState north = worldIn.getBlockState(pos.north());
+						IBlockState up = worldIn.getBlockState(pos.up());
+						IBlockState leftU = worldIn.getBlockState(pos.up().west());
+						IBlockState leftUNorth = worldIn.getBlockState(pos.up().west().north());
+						IBlockState northU = worldIn.getBlockState(pos.up().north());
+						if(left.getBlock() == this && left.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+							if(leftNorth.getBlock() == this && leftNorth.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+								if(north.getBlock() == this && north.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+									if(up.getBlock() == this && up.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+										if(leftU.getBlock() == this && leftU.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+											if(leftUNorth.getBlock() == this && leftUNorth.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+												if(northU.getBlock() == this && northU.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+													worldIn.createExplosion(playerIn, pos.getX() + 1, pos.getY() + 1, pos.getZ() + 1, 5.0f, true);
+													if(!playerIn.capabilities.isCreativeMode){
+														held.damageItem(1, playerIn);
+													}
+													return true;
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+					if(i == 7){
+						IBlockState right = worldIn.getBlockState(pos.east());
+						IBlockState rightNorth = worldIn.getBlockState(pos.east().north());
+						IBlockState north = worldIn.getBlockState(pos.north());
+						IBlockState up = worldIn.getBlockState(pos.up());
+						IBlockState rightU = worldIn.getBlockState(pos.up().east());
+						IBlockState rightUNorth = worldIn.getBlockState(pos.up().east().north());
+						IBlockState northU = worldIn.getBlockState(pos.up().north());
+						if(right.getBlock() == this && right.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+							if(rightNorth.getBlock() == this && rightNorth.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+								if(north.getBlock() == this && north.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+									if(up.getBlock() == this && up.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+										if(rightU.getBlock() == this && rightU.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+											if(rightUNorth.getBlock() == this && rightUNorth.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+												if(northU.getBlock() == this && northU.getValue(TYPE) == CompressedBlockType.GUNPOWDER){
+													worldIn.createExplosion(playerIn, pos.getX() + 1, pos.getY() + 1, pos.getZ() - 1, 5.0f, true);
+													if(!playerIn.capabilities.isCreativeMode){
+														held.damageItem(1, playerIn);
+													}
+													return true;
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}			
+		}
+        return false;
+    }
 	
 	@Override
 	public Material getMaterial(IBlockState state){
